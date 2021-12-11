370089731f4a1a0c1a069b12b9b39544af65bef8
Marcos Spomberg <marcosspomberg@gmail.com>
2021-12-11 00:12:29 +0000

    Implement character counter, make counter turn red when the character limit is reached

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#tweet-text").on('input', function () {
+    const charCounter = 140 - $(this).val().length;
+    const counter = $(this).parents().find('.counter');
+    counter.html(charCounter);
+    counter.text() < 0 ? counter.attr('id', "negative-counter") : counter.removeAttr('id', "negative-counter");
+  })
+});
