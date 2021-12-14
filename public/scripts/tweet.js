ece759dd6b7c695e36c81b4cc675f4405a8445e2
Marcos Spomberg <marcosspomberg@gmail.com>
2021-12-14 20:43:15 +0000

    Fix issue that prevents the tweets and buttons from changing while hovering over them

--- public/scripts/tweet.js
+++ public/scripts/tweet.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
   // Adds box-shadow while hovering over the tweet box
-  $('.tweet').hover(function() {
-    $(this).css('box-shadow', '5px 10px #afc4e3');
-  }, 
-    function() {
-      $(this).css('box-shadow', '');
-    }
-  )
+  $("#tweets-container").on("mouseover", ".tweet", function() {
+    $(this).css('box-shadow', '5px 10px #afc4e3'); 
+  })
+  
+  // Removes box-shadow once the mouse leaves the tweet
+  $("#tweets-container").on("mouseleave", ".tweet", function() {
+    $(this).css('box-shadow', ''); 
+  })
 
   // Changes the color of the icons while hovering over them
-  $(".icons button").hover(function() {
+  $("#tweets-container").on("mouseover", "button", function() {
     $(this).css('color', '#a19a40');
-  },
-    function() {
-      $(this).css('color', '#4056A1');
-    }
-  )
+  })
+
+  // Changes the color back once the mouse leaves the button
+  $("#tweets-container").on("mouseleave", "button", function() {
+    $(this).css('color', '#4056A1'); 
+  })
 });
