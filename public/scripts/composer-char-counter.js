2f3ab2b9c30157a36b43f76b901d883dd62a584f
Marcos Spomberg <marcosspomberg@gmail.com>
2021-12-21 22:59:08 +0000

    Add comment

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  // Changes the character counter based on the length of the data in the text area,
+  // also adds a negative counter ID when the length goes over 140 that turns the font red based on CSS settings. 
   $("#tweet-text").on('input', function() {
     const charCounter = 140 - $(this).val().length;
     const counter = $(this).parents().find('.counter');
